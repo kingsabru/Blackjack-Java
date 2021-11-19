037bb766adff77a71dafa61ffeabdf410447c006
Kingsley Abru <kingsleyabru11@outlook.com>
2021-11-19 16:28:09 +0000

    Refactored getTotal method

--- src/java/com/company/Hand.java
+++ src/java/com/company/Hand.java
@@ -6,24 +6,18 @@ import java.util.List;
 public class Hand {
     private List<Card> cards;
     private int numOfCards;
-    private int total;
+
 
     public Hand(){
         cards = new ArrayList<>(10);
         numOfCards = 0;
-        total = 0;
     }
 
     public void addCard(Card card){
         cards.add(card);
-        incrementTotal(card.getValue());
-    }
-
-    private void incrementTotal(int value){
-        total+= value;
     }
 
     public int getTotal() {
-        return total;
+        return cards.stream().mapToInt(Card::getValue).sum();
     }
 }
